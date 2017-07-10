b61a2c35bc77f1642172ab9564685991aa07a1bc
AnastasiaKravchuk <anastasija.kravchuk@gmail.com>
2017-07-10 19:47:27 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,10 +14,23 @@ namespace ConsoleUI
             double[] arr = { 1, 0, -2, -3, 5, 0, 7 };
             double[] arr1 = { 4, 0, 3, 2, 2};
             double[] arr2 = { 4, 0, 3, 2, 2 };
+            double[] arr3 = { 1, 2 , 0.000001};
+            double[] arr4 = { 0, 1 , 0.002};
             Polynomial obj = new Polynomial(arr);
             Polynomial obj1 = new Polynomial(arr1);
             Polynomial obj2 = new Polynomial(arr2);
-
+            Polynomial obj3 = new Polynomial(arr3, 0.001);
+            Polynomial obj4 = new Polynomial(arr4, 0.000003);
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Полиномы:");
+            Console.WriteLine("----------------------------------------------------");
+            obj3.toString();
+            obj4.toString();
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Умножение:");
+            Console.WriteLine("----------------------------------------------------");
+            (obj3*obj4).toString();
+            Console.WriteLine();
             obj.toString();
             Console.WriteLine();
             Console.WriteLine("Cумма:");
--- PolynomialExtension/Polynomial.cs
+++ PolynomialExtension/Polynomial.cs
@@ -9,6 +9,7 @@ namespace Logic
     public class Polynomial
     {
         private double[] coeff;
+        private double accuracy;
 
         public Polynomial(){}
 
@@ -17,9 +18,28 @@ namespace Logic
             SetCoefficients(arr);
         }
 
+        public Polynomial(double[] arr, double eps)
+        {
+            SetCoefficients(arr, eps);
+        }
+
         private void SetCoefficients(double[] arr)
         {
             coeff = arr;
+        }
+
+        private void SetCoefficients(double[] arr, double eps)
+        {
+            double[] res = new double[arr.Length];
+            accuracy = eps;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > accuracy)
+                {
+                    res[i] = arr[i];
+                }
+            }
+            coeff = res;
         }
 
         private double[] GetCoefficients()
@@ -39,33 +59,41 @@ namespace Logic
             double[] arr = coeff;
             for (int i = n - 1; i >= 0 ; i--)
             {
-                str += arr[i].ToString();
-                str += " x^";
-                str += i.ToString();
-                if(i > 0)
+                if (arr[i] > accuracy)
                 {
-                    str += " + ";
+                    str += arr[i].ToString();
+                    str += " x^";
+                    str += i.ToString();
+                    if (i > 0 && arr[i-1] > accuracy)
+                    {
+                        str += " + ";
+                    }
                 }
             }
             Console.WriteLine(str);
         }
 
-        public static Polynomial operator +(Polynomial first, Polynomial second)
+        public static Polynomial operator +(Polynomial lhs, Polynomial rhs)
         {
-            int l = Math.Max(first.GetDegree(), second.GetDegree());
+            int l = Math.Max(lhs.GetDegree(), rhs.GetDegree());
             
             double[] sum = new double[l];
             double temp1, temp2;
             for (int i = 0; i < l; i++)
             {
-                if (i < first.GetDegree()) { temp1 = first.GetCoefficients()[i]; }
+                if (i < lhs.GetDegree()) { temp1 = lhs.GetCoefficients()[i]; }
                     else { temp1 = 0; }
 
-                if (i < second.GetDegree()) { temp2 = second.GetCoefficients()[i]; }
+                if (i < rhs.GetDegree()) { temp2 = rhs.GetCoefficients()[i]; }
                     else { temp2 = 0; }
                 sum[i] = temp1 + temp2;
             }
             return new Polynomial(sum);
+        }
+
+        public static Polynomial Add(Polynomial lhs, Polynomial rhs)
+        {
+            return lhs + rhs;
         }
 
         public static Polynomial operator -(Polynomial first, Polynomial second)
@@ -84,6 +112,47 @@ namespace Logic
                 sum[i] = temp1 - temp2;
             }
             return new Polynomial(sum);
+        }
+
+        public static Polynomial Subtract(Polynomial lhs, Polynomial rhs)
+        {
+            return lhs - rhs;
+        }
+
+        public static Polynomial operator *(Polynomial obj, double constant)
+        {
+            int n = obj.GetDegree();
+            double[] res = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                res[i] *= constant;
+            }
+            return new Polynomial(res);
+        }
+
+        public static Polynomial MultiplyConst(Polynomial obj, double k)
+        {
+            return obj * k;
+        }
+
+        public static Polynomial Multiply(Polynomial lhs, Polynomial rhs)
+        {
+            return lhs*rhs;
+        }
+
+        public static Polynomial operator *(Polynomial obj1, Polynomial obj2)
+        {
+            int l1 = obj1.GetDegree(); int l2 = obj2.GetDegree();
+            int l = l1 * l2;
+            double[] res = new double[l];
+            for (int i = 0; i < l1; i++)
+            {
+                for (int j = 0; j < l2; j++)
+                {
+                    res[i+j] += obj1.GetCoefficients()[i]*obj2.GetCoefficients()[j];
+                }
+            }
+            return new Polynomial(res);
         }
 
         public bool Equals(Polynomial obj)
